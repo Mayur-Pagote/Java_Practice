93937755b4818f5467bfaff8d79c21b74ddfb253
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-12 21:10:31 +0530

    Create Table of 19.java

---
+++ Loops/Table of 19.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      for (int i=0; i<=10; i++){
+        System.out.print("19 * "+i);
+        System.out.print(" = ");
+        System.out.print(i * 19);
+        System.out.println("");
+        
+      }
+  }
+}
