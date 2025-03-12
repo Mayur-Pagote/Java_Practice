892f19e9a852632167f7c7a83eef87cf6125e1c7
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-12 11:43:35 +0530

    Create Sum of 2 numbers.java

---
+++ Input in Java/Sum of 2 numbers.java
@@ -0,0 +1,13 @@
+package javaInput;
+import java.util.Scanner;
+
+public class Main {
+  public static void main (String [] args) {
+    Scanner sc = new Scanner(System.in);
+    int num1, num2;
+    num1 = sc.nextInt();
+    num2 = sc.nextInt();
+    System.out.print(num1 +num2);
+    
+  }
+}
