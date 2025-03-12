70c13b2d857bd9ea77a813425b5f698912e0d934
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-12 11:19:56 +0530

    Create Percentage of 5 subjects.java

---
+++ Basics of Java/Percentage of 5 subjects.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main (String [] args){
+    double phy, chem, math, comp, eng, res;
+    phy = 37.5;
+    chem = 70.2;
+    math = 40;
+    comp = 90.9;
+    eng = 91.0;
+    res = ((phy + chem + math + comp + eng) / 5 );
+    System.out.println("Percentage is "+res);
+    
+  }
+}
