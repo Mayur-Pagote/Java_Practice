def002f8f106284cf721d43ff2715e49f6a9f4f8
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-13 10:03:13 +0530

    Create Hello.java

---
+++ Functions/Hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void hello (){
+      System.out.println("Hello");
+    }
+  
+    public static void main(String[] args) {
+      hello();
+  }
+}
