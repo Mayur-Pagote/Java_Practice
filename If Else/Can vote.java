5ab247f9aadb839ada87949ada5b924eda645757
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-27 11:31:46 +0530

    Create Can vote.java

---
+++ If Else/Can vote.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+
+public class Main {
+    public static void main(String[] args) {
+      int age = 18;
+      
+      if (age >= 18) System.out.println("Can Vote");
+      else System.out.println("Cannot vote");
+      
+  }
+}
