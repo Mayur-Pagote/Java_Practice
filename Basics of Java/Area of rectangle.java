517853095dafefa39f9f10729ce36c30a366279c
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-30 09:54:09 +0530

    Create Area of rectangle.java

---
+++ Basics of Java/Area of rectangle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int num1, num2;
+      num1 = 10;
+      num2 = 5;
+      System.out.println("Area of Rectangle is: "+num1*num2);
+  }
+}
