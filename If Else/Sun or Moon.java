1310cf8fab3c43ba26ed96e944ceb78972a19187
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-04-08 17:30:49 +0530

    Create Sun or Moon.java

---
+++ If Else/Sun or Moon.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      String x = "Sun";
+      if ( x == "Sun") System.out.println("1");
+      else System.out.println("0");
+  }
+}
